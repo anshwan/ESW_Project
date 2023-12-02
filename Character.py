36b07f5fabc2eb054d6a41f6b4e14a4b64057765
anshwan <ansh10041@gmail.com>
2023-12-02 23:28:02 +0900

    bullet add

--- Character.py
+++ Character.py
@@ -5,36 +5,40 @@ import numpy as np
 class Stars:
     def __init__(self, width, height):
         self.stand = Image.open('./images/star.png').convert("RGBA")
-        #self.appearance = self.stand.transform(self.stand.size, Image.AFFINE, (1,0,0,0,1,-70))
         self.appearance = self.stand.resize((128,128))
-        #self.position = np.array([width/2 - 20, height/2 - 20, width/2 + 20, height/2 + 20])
         self.position_x = int(width // 2 - self.appearance.width/2)
         self.position_y = int(height // 2 - self.appearance.height/2 + 90)
+        self.center = np.array([self.position_x + self.appearance.width / 2, self.position_y + self.appearance.height / 2])
         self.state = None
 
+    #def calculate_center(self):
+        #self.center_x = int(self.width  / 2)
+        #self.center_y = int(self.height / 2)
+        #self.position_x = int(self.center_x - self.appearance.width / 2)
+        #self.position_y = int(self.center_y - self.appearance.height / 2)
+
     def move(self, command = None):
-        if command == None:
+        if command['move'] == False:
             self.state = None
         
-        else:
-            self.state = 'move'
+        else:   
+            self.state = 'move' 
 
-            if command == 'up_pressed':
+            if command['up_pressed']:
                 self.position_y -= 5
                 
 
-            elif command == 'down_pressed':
+            if command['down_pressed']:
                 self.position_y += 5
                 
 
-            elif command == 'left_pressed':
+            if command['left_pressed']:
                 self.position_x -= 5
                
                 
-            elif command == 'right_pressed':
+            if command['right_pressed']:
                 self.position_x += 5
-            
+            self.center = np.array([self.position_x + self.appearance.width / 2, self.position_y + self.appearance.height / 2])
 
     def draw_star(self, draw, background):
         background.paste(self.appearance, (self.position_x, self.position_y), self.appearance)
-        #draw.bitmap((self.position_x, self.position_y), self.appearance, fill=(255,255,255,100), mask = self.appearance)
--- main.py
+++ main.py
@@ -1,35 +1,67 @@
+import time
+import random
+import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from Character import Stars
 from MyBoard import Display
+from bullet import Bullet
 
 def main(Display):
     my_image = Image.new("RGBA", (Display.width, Display.height))
     my_draw = ImageDraw.Draw(my_image)
+
     my_draw.rectangle((0, 0, Display.width, Display.height))
     Display.disp.image(my_image)
 
     # 잔상이 남지 않는 코드
     my_star = Stars(Display.width, Display.height)  # Character 대신에 my_star로 이름 변경
 
+    #불렛 추가
+    bullets=[]
+
     while True:
-        command = None
+        command = {'move': False, 'up_pressed': False, 'down_pressed': False, 'left_pressed': False, 'right_pressed': False}
+
         if not Display.button_U.value:  # up pressed
-            command = 'up_pressed'
-        elif not Display.button_D.value:  # down pressed
-            command = 'down_pressed'
-        elif not Display.button_L.value:  # left pressed
-            command = 'left_pressed'
-        elif not Display.button_R.value:  # right pressed
-            command = 'right_pressed'
-        else:
-            command = None
+            command['up_pressed'] = True
+            command['move'] = True
+        if not Display.button_D.value:  # down pressed
+            command['down_pressed'] = True
+            command['move'] = True
+        if not Display.button_L.value:  # left pressed
+            command['left_pressed'] = True
+            command['move'] = True
+        if not Display.button_R.value:  # right pressed
+            command['right_pressed'] = True
+            command['move'] = True
+        #
+        if not Display.button_A.value:
+            bullet = Bullet(my_star.center, command)
+            bullets.append(bullet)
+
 
         my_star.move(command)
+        #
+        for bullet in bullets:
+            bullet.move()
+        #else:
+            #command = None
+
+        #my_star.move(command)
 
         # 그리는 순서가 중요합니다. 배경을 먼저 깔고 위에 그림을 그리고 싶었는데
         # 그림을 그려놓고 배경으로 덮는 결과로 될 수 있습니다.
         my_draw.rectangle((0, 0, int(Display.width), int(Display.height)), fill=(0, 0, 0, 100))
         my_star.draw_star(my_draw, my_image)  # my_circle 대신에 my_star로 변경
+
+
+        #
+        for bullet in bullets:
+            if bullet.state != 'hit':
+                my_draw.rectangle(tuple(bullet.position), outline = bullet.outline, fill = (0,0,225))
+        
+
+
         Display.disp.image(my_image)
 
 if __name__ == '__main__':
