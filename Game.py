270fab083f9a660d34552e5db42380fff08caa2a
anshwan <ansh10041@gmail.com>
2023-12-06 02:45:51 +0900

    code update

---
+++ Game.py
@@ -0,0 +1,57 @@
+import time
+import random
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+
+def game_over(draw, image, Display):
+    game_over_fnt = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 38)   
+    bg_image = Image.open('./images/space.png').convert('RGBA')
+    bg_image_resize = bg_image.resize((240 ,240))
+    while True:
+        draw.rectangle((0, 0, Display.width, Display.height))
+        image.paste(bg_image_resize, (0,0), bg_image_resize)
+        draw.text((3, 100), "GAME OVER", font=game_over_fnt, fill=(255,255,255))
+        Display.disp.image(image)
+
+def stage_clear(stage, get_candy, stage_candy):
+    if(get_candy[0] >= stage_candy[0] and get_candy[1] >= stage_candy[1] and get_candy[2] >= stage_candy[2]):
+        return True
+    return False
+
+def stage_clear_display(draw, image, Display, star, stage):
+    stage_clear_fnt = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 30)
+    bg_image = Image.open('./images/space.png').convert('RGBA')
+    bg_image_resize = bg_image.resize((240 ,240))
+    #star_image = Image.open('./images/starcandy_red.png').convert("RGBA")
+    #star_resize = star_image.resize((150,150))
+    draw.rectangle((0, 0, Display.width, Display.height))
+    image.paste(bg_image_resize, (0,0), bg_image_resize)
+    star.draw_star(draw, image)
+    draw.text((3, 100), f"STAGE {stage} CLAER", font=stage_clear_fnt, fill=(255,255,255))
+    Display.disp.image(image)
+    time.sleep(2)
+
+def new_stage_display(my_image, my_draw, stage, get_candy, stage_candy, red_candy, blue_candy, yellow_candy, fnt):
+    my_draw.text((0, 0), f"STAGE {stage}", font=fnt, fill=(255, 255, 255))
+    my_image.paste(red_candy.appearance, (-35, 15), red_candy.appearance)
+    my_draw.text((10, 30), f"{get_candy[0]}/{stage_candy[0]}", font=fnt, fill=(255, 255, 255))
+    my_image.paste(blue_candy.appearance, (0, 15), blue_candy.appearance)
+    my_draw.text((45, 30), f"{get_candy[1]}/{stage_candy[1]}", font=fnt, fill=(255, 255, 255))
+    my_image.paste(yellow_candy.appearance, (35, 15), yellow_candy.appearance)
+    my_draw.text((80, 30), f"{get_candy[2]}/{stage_candy[2]}", font=fnt, fill=(255, 255, 255))
+
+def addcandy(stage, stage_candy):
+    stage_candy=[]
+    if(stage == 2):
+        for _ in range(3):
+            random_number = random.randint(5, 7)
+            stage_candy.append(random_number)
+
+    if(stage == 3):
+        for _ in range(3):
+            random_number = random.randint(7, 9)
+            stage_candy.append(random_number)
+    
+    return stage_candy
+
--- main.py
+++ main.py
@@ -7,23 +7,24 @@ from MyBoard import Display
 from bullet import Bullet
 from starcandy import RedStarCandy, BlueStarCandy, YellowStarCandy, RainbowStarCandy
 from enemy import Asteroid, Meteor, Missile
+import Game as game
 
 def main(Display):
     my_image = Image.new("RGBA", (Display.width, Display.height))
     my_draw = ImageDraw.Draw(my_image)
 
     #배경화면
-    background_image = Image.open('./images/space3.png').convert('RGBA')
+    background_image = Image.open('./images/space.png').convert('RGBA')
     re_background_image = background_image.resize((240 ,240))
     my_image.paste(re_background_image, (0,0), re_background_image)
 
     #my_draw.rectangle((0, 0, Display.width, Display.height))
     Display.disp.image(my_image)
 
-    # 잔상이 남지 않는 코드
+    # 별 캐릭터 추가  
     my_star = Stars(Display.width, Display.height)  
 
-    #불렛 추가
+    # 불렛 추가
     bullets=[]
 
     # 적 추가   
@@ -39,21 +40,28 @@ def main(Display):
     rainbow_candy = RainbowStarCandy()
     candies = [red_candy, blue_candy, yellow_candy, rainbow_candy]
 
+    # stage
+    stage = 1
+    # stage clear state 
+    Isclear = False
     
     end_time = time.time()
 
     fnt = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 15)
 
-    round1_candy = []   
+    stage_candy = []   
     for _ in range(3):
         random_number = random.randint(3, 5)
-        round1_candy.append(random_number)
-    get_blue = 0
-    get_red = 0
-    get_yellow = 0
+        stage_candy.append(random_number)
+
+    # 획득한 red, blue, yellow candy의 개수
+    get_candy = [0, 0, 0] 
 
     
     while True:
+        if (my_star.life <= 0):
+            game.game_over(my_draw, my_image, disp)
+                
 
         command = {'move': False, 'up_pressed': False, 'down_pressed': False, 'left_pressed': False, 'right_pressed': False}
 
@@ -78,8 +86,9 @@ def main(Display):
         #draw = ImageDraw.Draw(background)
 
 
+        # 랜덤한 적과 사탕 생성 후 떨어트리는 코드
         current_time = time.time()
-        if current_time - end_time >= 5:
+        if current_time - end_time >= 7:
             enemy_type = random.choice([Asteroid, Meteor, Missile])
             enemies.append(enemy_type())
             for _ in range (2):
@@ -89,6 +98,8 @@ def main(Display):
 
         my_star.move(command)
 
+
+        # 적과의 충돌 시 EVENT
         collided_enemy = my_star.check_collision(enemies)
         if collided_enemy:
             if isinstance(collided_enemy, Missile):
@@ -97,18 +108,17 @@ def main(Display):
                 my_star.life -= 1
             time.sleep(0.01)
 
+        # 별사탕과의 충돌 시 EVENT
         collided_candy = my_star.check_collision(candies)
         if collided_candy:
             if isinstance(collided_candy, RedStarCandy):
-                get_red += 1
+                get_candy[0]+= 1
             elif isinstance(collided_candy, BlueStarCandy):
-                get_blue += 1
+                get_candy[1] += 1
             elif isinstance(collided_candy, YellowStarCandy):
-                get_yellow += 1
+                get_candy[2] += 1
             elif isinstance(collided_candy, RainbowStarCandy):
-                get_red += 1
-                get_blue += 1
-                get_yellow += 1
+                get_candy = [x + 1 for x in get_candy]
             time.sleep(0.01)
 
 
@@ -119,7 +129,7 @@ def main(Display):
 
 
 
-        # LIFE DISPLAY
+        # 하트 DISPLAY
         x_offset = -35
         for i in range(my_star.life) :
             my_image.paste(my_star.life_image_resize,(x_offset, -65) , my_star.life_image_resize)
@@ -127,13 +137,33 @@ def main(Display):
         my_draw.text((0, 15), "LIFE ", font=fnt, fill=(255,255,255))  
 
 
-        # HAVE TO GET CANDY DISPLAY
+        # STAGE & 먹어야 하는 사탕 DISPLAY 
+        my_draw.text((0,0), f"STAGE {stage}", font=fnt, fill=(255,255,255))
         my_image.paste(red_candy.appearance, (-35, 15), red_candy.appearance)
-        my_draw.text((10, 30), f"{get_red}/{round1_candy[0]}" , font = fnt, fill=(255,255,255))
+        my_draw.text((10, 30), f"{get_candy[0]}/{stage_candy[0]}" , font = fnt, fill=(255,255,255))
         my_image.paste(blue_candy.appearance, (0, 15), blue_candy.appearance)
-        my_draw.text((45,30), f"{get_blue}/{round1_candy[1]}", font = fnt, fill = (255,255,255))
+        my_draw.text((45,30), f"{get_candy[1]}/{stage_candy[1]}", font = fnt, fill = (255,255,255))
         my_image.paste(yellow_candy.appearance, (35, 15), yellow_candy.appearance)
-        my_draw.text((80,30), f"{get_yellow}/{round1_candy[2]}", font = fnt, fill = (255,255,255))
+        my_draw.text((80,30), f"{get_candy[2]}/{stage_candy[2]}", font = fnt, fill = (255,255,255))
+
+        #stage를 clear 했는지 확인
+        Isclear = game.stage_clear(stage, get_candy, stage_candy)
+
+        #stage를 clear 한 경우 다음 stage로 이동
+        if(Isclear):
+            game.stage_clear_display(my_draw, my_image, Display, my_star, stage)
+            if(stage == 3):
+                complete_fnt = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+                while True:
+                    my_image.paste(re_background_image, (0,0), re_background_image)
+                    my_draw.text((3, 100), "CONGRATULATION!", font=complete_fnt, fill=(255,255,255))
+                    my_image.paste(my_star.appearance, (56,146), my_star.appearance)
+                    Display.disp.image(my_image)
+            stage += 1
+            get_candy = [0,0,0]
+            stage_candy = game.addcandy(stage, stage_candy)
+            game.new_stage_display(my_image, my_draw, stage, get_candy, stage_candy, red_candy, blue_candy, yellow_candy, fnt)
+            
 
 
         for enemy in enemies:
@@ -147,12 +177,6 @@ def main(Display):
 
         for bullet in bullets:
             bullet.move()
-        
-        # 그리는 순서가 중요합니다. 배경을 먼저 깔고 위에 그림을 그리고 싶었는데
-        # 그림을 그려놓고 배경으로 덮는 결과로 될 수 있습니다.
-        #my_draw.rectangle((0, 0, int(Display.width), int(Display.height)), fill=(0, 0, 0, 100))
-        #my_image.paste(re_background_image, (0,0))
-        #my_star.draw_star(my_draw, my_image)  # my_circle 대신에 my_star로 변경
 
         for bullet in bullets:
             if bullet.state != 'hit':
@@ -160,6 +184,11 @@ def main(Display):
 
         Display.disp.image(my_image)
         time.sleep(0.01)
+
+        #my_draw.rectangle((0, 0, int(Display.width), int(Display.height)), fill=(0, 0, 0, 100))
+        #my_image.paste(re_background_image, (0,0))
+        #my_star.draw_star(my_draw, my_image)
+
 
 if __name__ == '__main__':
     disp = Display()
